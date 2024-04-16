c5ad31fb3566fc3bff99d4817f6f33b0837a96a8
Tomas <tomascanepa1397@gmail.com>
2024-04-16 19:32:58 -0300

    making list 2 (an ok stack) generic

--- src/second.rs
+++ src/second.rs
@@ -1,25 +1,25 @@
 // List 2: an ok singly-linked stack
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
 // type alias to not have to constantly write 'Option<Box<Node>>'
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     // static method (doesn't take self as an argument)
     // New
     pub fn new() -> Self {
         List { head: None }
     }
     // Push operation
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
             next: self.head.take(),
@@ -27,7 +27,7 @@ impl List {
         self.head = Some(new_node);
     }
     // Pop operation
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -35,7 +35,7 @@ impl List {
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
         // 'while let' = do this until the pattern doesn't match
