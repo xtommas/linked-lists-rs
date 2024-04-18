d48780f272ed6fb9e2cf40c7c45e7a27e02f451e
Tomas <tomascanepa1397@gmail.com>
2024-04-18 10:50:05 -0300

    list 2: IntoIter

--- src/second.rs
+++ src/second.rs
@@ -12,6 +12,8 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     // static method (doesn't take self as an argument)
     // New
@@ -40,6 +42,17 @@ impl<T> List<T> {
     // Mutable peek
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
+    }
+    // Iterator
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -70,4 +83,17 @@ fn peek() {
 
     assert_eq!(list.peek(), Some(&42));
     assert_eq!(list.pop(), Some(42));
+}
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
 }
