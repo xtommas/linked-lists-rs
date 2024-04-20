1be0620887a33ae46de715288813a3f7fe431828
Tomas <tomascanepa1397@gmail.com>
2024-04-20 11:31:47 -0300

    list 2: an ok stack

--- src/second.rs
+++ src/second.rs
@@ -18,6 +18,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
     // static method (doesn't take self as an argument)
     // New
@@ -57,6 +61,12 @@ impl<T> List<T> {
             next: self.head.as_deref(),
         }
     }
+    // IterMut
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -73,6 +83,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
         self.next.map(|node| {
             self.next = node.next.as_deref();
             &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
         })
     }
 }
@@ -129,4 +150,16 @@ fn iter() {
     assert_eq!(iter.next(), Some(&3));
     assert_eq!(iter.next(), Some(&2));
     assert_eq!(iter.next(), Some(&1));
+}
+
+#[test]
+fn iter_mut() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    let mut iter = list.iter_mut();
+    assert_eq!(iter.next(), Some(&mut 3));
+    assert_eq!(iter.next(), Some(&mut 2));
+    assert_eq!(iter.next(), Some(&mut 1));
 }
