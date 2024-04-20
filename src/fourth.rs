1fa4c1101c0f7f3155509658cbbb8ed9cef2d696
Tomas <tomascanepa1397@gmail.com>
2024-04-20 17:05:03 -0300

    list 4: layout

---
+++ src/fourth.rs
@@ -0,0 +1,15 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
