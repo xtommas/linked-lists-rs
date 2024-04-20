17bee9dab5f1b21ba7ef409ce824dff66c4952ce
Tomas <tomascanepa1397@gmail.com>
2024-04-20 17:22:22 -0300

    list 4: push front

--- src/fourth.rs
+++ src/fourth.rs
@@ -13,3 +13,39 @@ struct Node<T> {
     next: Link<T>,
     prev: Link<T>,
 }
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs 2 links, everything else should be 0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old head
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+}
